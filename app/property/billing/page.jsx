7043a1cc644947e7a82067d5af98f95b446f95cd
Amit Mahapatra <connectamit23@gmail.com>
2025-05-27 16:27:46 +0530

    room booking filter updated

--- app/property/billing/page.jsx
+++ app/property/billing/page.jsx
@@ -214,7 +214,7 @@ export default function Billing() {
 
   const handleCheckIn = async (bill) => {
     try {
-      setCheckInOutLoadingId(bill.currentBillingId);
+      setCheckInOutLoadingId(bill.billing._id);
 
       const payload = {
         ...bill.booking,
@@ -270,7 +270,7 @@ export default function Billing() {
 
   const handleCheckOut = async (bill) => {
     try {
-      setCheckInOutLoadingId(bill.currentBillingId);
+      setCheckInOutLoadingId(bill.billing._id);
 
       const payload = {
         ...bill.booking,
@@ -331,7 +331,7 @@ export default function Billing() {
     const today = new Date();
     checkInDate.setHours(0, 0, 0, 0);
     today.setHours(0, 0, 0, 0);
-    if (checkInDate.getTime() <= today.getTime()) {
+    if (checkInDate.getTime() >= today.getTime()) {
       return true; // Allow check-in if today is the check-in date
     }
     return false; // Do not allow check-in if it's not the check-in date
@@ -351,7 +351,7 @@ export default function Billing() {
         )}
         <div className="container mx-auto py-10" style={{ maxWidth: "85%" }}>
           <div className="flex justify-between items-center mb-4">
-            {/* <Box sx={{ display: "flex", gap: 1 }}>
+            <Box sx={{ display: "flex", gap: 1 }}>
               <Button
                 variant={filterStatus === "all" ? "contained" : "outlined"}
                 color="primary"
@@ -359,20 +359,7 @@ export default function Billing() {
               >
                 All
               </Button>
-              <Button
-                variant={filterStatus === "yes" ? "contained" : "outlined"}
-                color="primary"
-                onClick={() => setFilterStatus("yes")}
-              >
-                Paid
-              </Button>
-              <Button
-                variant={filterStatus === "no" ? "contained" : "outlined"}
-                color="primary"
-                onClick={() => setFilterStatus("no")}
-              >
-                UnPaid
-              </Button>
+
               <Button
                 variant={filterStatus === "Booked" ? "contained" : "outlined"}
                 color="primary"
@@ -407,7 +394,7 @@ export default function Billing() {
               >
                 Cancelled
               </Button>
-            </Box> */}
+            </Box>
             <Link href="roomdashboard/newguest">
               <Button
                 variant="contained"
@@ -440,68 +427,100 @@ export default function Billing() {
               </TableHead>
               <TableBody>
                 {billingData.length > 0 ? (
-                  billingData.map((bill, index) => {
-                    const bookingDate = GetCustomDate(bill.booking.createdAt);
-                    const checkInDate = GetCustomDate(bill.booking.checkIn);
-                    const checkOutDate = GetCustomDate(bill.booking.checkOut);
+                  billingData
+                    .filter((bill) => {
+                      if (filterStatus === "all") return true;
+                      if (filterStatus === "Booked") {
+                        return (
+                          bill.booking.CheckedIn === false &&
+                          bill.booking.CheckedOut === false &&
+                          bill.billing.Cancelled !== "yes"
+                        );
+                      }
+                      if (filterStatus === "Checked In") {
+                        return (
+                          bill.booking.CheckedIn === true &&
+                          bill.booking.CheckedOut === false &&
+                          bill.billing.Cancelled !== "yes"
+                        );
+                      }
+                      if (filterStatus === "Checked Out") {
+                        return (
+                          bill.booking.CheckedIn === true &&
+                          bill.booking.CheckedOut === true &&
+                          bill.billing.Cancelled !== "yes"
+                        );
+                      }
+                      if (filterStatus === "Cancelled") {
+                        return bill.billing.Cancelled === "yes";
+                      }
+                      return true; // Default case, should not happen
+                    })
+                    .map((bill, index) => {
+                      const bookingDate = GetCustomDate(bill.booking.createdAt);
+                      const checkInDate = GetCustomDate(bill.booking.checkIn);
+                      const checkOutDate = GetCustomDate(bill.booking.checkOut);
 
-                    const isCheckInAllowed = handleAllowCheckIn(bill);
-                    return (
-                      <TableRow
-                        key={index}
-                        sx={{
-                          "& > td": {
-                            backgroundColor: "white",
-                            textAlign: "center",
-                          },
-                          background: `linear-gradient(to right, ${
-                            bill.billing.Cancelled === "yes"
-                              ? "#808080"
-                              : bill.billing.Bill_Paid === "yes"
-                              ? "#1ebc1e"
-                              : "#f24a23"
-                          } 5%, white 5%)`,
-                        }}
-                      >
-                        <CustomBodyCell>{index + 1}</CustomBodyCell>
-                        <CustomBodyCell>
-                          <Link
-                            href={`/property/billing/guest-bill/${bill.billing._id}`}
-                            style={{
-                              color: " #00158a",
+                      const isCheckInAllowed = handleAllowCheckIn(bill);
+                      return (
+                        <TableRow
+                          key={index}
+                          sx={{
+                            "& > td": {
+                              backgroundColor: "white",
+                              textAlign: "center",
+                            },
+                            background: `linear-gradient(to right, ${
+                              bill.billing.Cancelled === "yes"
+                                ? "#808080"
+                                : bill.billing.Bill_Paid === "yes"
+                                ? "#1ebc1e"
+                                : "#f24a23"
+                            } 5%, white 5%)`,
+                          }}
+                        >
+                          <CustomBodyCell>{index + 1}</CustomBodyCell>
+                          <CustomBodyCell>
+                            <Link
+                              href={`/property/billing/guest-bill/${bill.billing._id}`}
+                              style={{
+                                color: " #00158a",
 
-                              fontWeight: "400",
-                            }}
-                          >
-                            {bill.billing.bookingId || "N/A"}
-                          </Link>
-                        </CustomBodyCell>
-                        <CustomBodyCell>
-                          {bill.booking.guestName || "N/A"} <br />
-                          {bill.booking.mobileNo || "N/A"}
-                        </CustomBodyCell>
-                        <CustomBodyCell sx={{ fontWeight: 600 }}>
-                          {Array.isArray(bill.billing.roomNo)
-                            ? bill.billing.roomNo.join(", ")
-                            : bill.billing.roomNo || "N/A"}
-                        </CustomBodyCell>
-                        <CustomBodyCell>
-                          {checkInDate || "N/A"}
-                          <br />
-                          {checkOutDate || "N/A"}
-                        </CustomBodyCell>
-                        <CustomBodyCell>{bookingDate || "N/A"}</CustomBodyCell>
-                        <CustomBodyCell>
-                          Adults: {bill?.booking?.adults || "N/A"} <br /> Child:
-                          {bill?.booking?.children}
-                        </CustomBodyCell>
-                        <CustomBodyCell>
-                          {bill?.booking?.mealPlan || "N/A"}
-                        </CustomBodyCell>
-                        <CustomBodyCell>
-                          {bill?.booking?.remarks || "N/A"}
-                        </CustomBodyCell>
-                        {/* <CustomBodyCell>
+                                fontWeight: "400",
+                              }}
+                            >
+                              {bill.billing.bookingId || "N/A"}
+                            </Link>
+                          </CustomBodyCell>
+                          <CustomBodyCell>
+                            {bill.booking.guestName || "N/A"} <br />
+                            {bill.booking.mobileNo || "N/A"}
+                          </CustomBodyCell>
+                          <CustomBodyCell sx={{ fontWeight: 600 }}>
+                            {Array.isArray(bill.billing.roomNo)
+                              ? bill.billing.roomNo.join(", ")
+                              : bill.billing.roomNo || "N/A"}
+                          </CustomBodyCell>
+                          <CustomBodyCell>
+                            {checkInDate || "N/A"}
+                            <br />
+                            {checkOutDate || "N/A"}
+                          </CustomBodyCell>
+                          <CustomBodyCell>
+                            {bookingDate || "N/A"}
+                          </CustomBodyCell>
+                          <CustomBodyCell>
+                            Adults: {bill?.booking?.adults || "N/A"} <br />{" "}
+                            Child:
+                            {bill?.booking?.children}
+                          </CustomBodyCell>
+                          <CustomBodyCell>
+                            {bill?.booking?.mealPlan || "N/A"}
+                          </CustomBodyCell>
+                          <CustomBodyCell>
+                            {bill?.booking?.remarks || "N/A"}
+                          </CustomBodyCell>
+                          {/* <CustomBodyCell>
                           ₹{parseFloat(bill.billing.totalAmount).toFixed(2) || 0}
                         </CustomBodyCell>
                         <CustomBodyCell>
@@ -511,15 +530,41 @@ export default function Billing() {
                         <CustomBodyCell>
                           ₹{parseFloat(bill.billing.dueAmount).toFixed(2) || 0}
                         </CustomBodyCell> */}
-                        <CustomBodyCell>
-                          {getGuestStatus(bill)}
-                          <br />
-                          {isCheckInAllowed && (
-                            <>
-                              {bill?.booking?.CheckedIn == false && (
+                          <CustomBodyCell>
+                            {getGuestStatus(bill)}
+                            <br />
+                            {isCheckInAllowed && (
+                              <>
+                                {bill?.booking?.CheckedIn == false && (
+                                  <Button
+                                    variant="contained"
+                                    color="success"
+                                    sx={{
+                                      textTransform: "none",
+                                      fontSize: "10px",
+                                      p: "0px 4px",
+                                      minWidth: 0,
+                                    }}
+                                    disabled={
+                                      checkInOutLoadingId ===
+                                      bill.currentBillingId
+                                    }
+                                    onClick={() => handleCheckIn(bill)}
+                                  >
+                                    {checkInOutLoadingId !==
+                                    bill.currentBillingId
+                                      ? "Check In"
+                                      : "Checking In..."}
+                                  </Button>
+                                )}
+                              </>
+                            )}
+
+                            {bill?.booking?.CheckedIn &&
+                              !bill?.booking?.CheckedOut && (
                                 <Button
                                   variant="contained"
-                                  color="success"
+                                  color="error"
                                   sx={{
                                     textTransform: "none",
                                     fontSize: "10px",
@@ -527,58 +572,33 @@ export default function Billing() {
                                     minWidth: 0,
                                   }}
                                   disabled={
-                                    checkInOutLoadingId ===
-                                    bill.currentBillingId
+                                    checkInOutLoadingId === bill.billing._id
                                   }
-                                  onClick={() => handleCheckIn(bill)}
+                                  onClick={() => handleCheckOut(bill)}
                                 >
-                                  {checkInOutLoadingId !== bill.currentBillingId
-                                    ? "Check In"
-                                    : "Checking In..."}
+                                  {checkInOutLoadingId !== bill.billing._id
+                                    ? "Check Out"
+                                    : "Checking Out..."}
                                 </Button>
                               )}
-                            </>
-                          )}
-
-                          {bill?.booking?.CheckedIn &&
-                            !bill?.booking?.CheckedOut && (
-                              <Button
-                                variant="contained"
-                                color="error"
-                                sx={{
-                                  textTransform: "none",
-                                  fontSize: "10px",
-                                  p: "0px 4px",
-                                  minWidth: 0,
-                                }}
-                                disabled={
-                                  checkInOutLoadingId === bill.currentBillingId
-                                }
-                                onClick={() => handleCheckOut(bill)}
-                              >
-                                {checkInOutLoadingId !== bill.currentBillingId
-                                  ? "Check Out"
-                                  : "Checking Out..."}
-                              </Button>
-                            )}
-                        </CustomBodyCell>
-                        {/* <CustomBodyCell>{getBillStatus(bill)}</CustomBodyCell> */}
-                        <CustomBodyCell>
-                          <Button
-                            variant="contained"
-                            color="warning"
-                            onClick={() => handleViewBill(bill)}
-                            sx={{
-                              p: "4px",
-                              minWidth: 0,
-                            }}
-                          >
-                            <SquarePen size={18} />
-                          </Button>
-                        </CustomBodyCell>
-                      </TableRow>
-                    );
-                  })
+                          </CustomBodyCell>
+                          {/* <CustomBodyCell>{getBillStatus(bill)}</CustomBodyCell> */}
+                          <CustomBodyCell>
+                            <Button
+                              variant="contained"
+                              color="warning"
+                              onClick={() => handleViewBill(bill)}
+                              sx={{
+                                p: "4px",
+                                minWidth: 0,
+                              }}
+                            >
+                              <SquarePen size={18} />
+                            </Button>
+                          </CustomBodyCell>
+                        </TableRow>
+                      );
+                    })
                 ) : (
                   <TableRow>
                     <TableCell colSpan={9} align="center">
